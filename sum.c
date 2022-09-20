dbc1f1b3f70179cdef3c49fcea86f15d6037ecad
prince kumar <113275797+prince21212@users.noreply.github.com>
2022-09-20 22:09:52 +0530

    Adding two numbers using C programming

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    int a, b, sum;
+    printf("enter the number\n");
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    printf("Sum of %d + %d = %d", a, b, sum);
+}
