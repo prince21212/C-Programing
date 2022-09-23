4ee3918c0a41e6af77c81f2f8dc6406944ab5fa3
prince kumar <113275797+prince21212@users.noreply.github.com>
2022-09-23 22:27:04 +0530

    using loop and switch in c programming
    changing one value into another value :
     kmsToMiles
     inchesToFoot 
     cmsToInches 
     poundToKgs
     inchesToMeters

---
+++ exercise2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i;
+    float b;
+    while (5)
+    {
+        printf("1.kmsTomiles\n2.inchesTofoot\n3.cmsToinches\n4.poundTokgs\n5.inchesTometers\n");
+        printf("press the number you want use\n");
+        scanf("%d", &i);
+
+        switch (i)
+        {
+
+        case 0:
+            printf("Quitting the program...");
+
+            goto end;
+            break;
+        case 1:
+            printf("*******************Kms To Miles*******************\n");
+            printf("Enter the value which you want to convert\n");
+            scanf("%f", &b);
+            printf("%d kms = %f miles\n", (int)b, b * 0.621371);
+
+            break;
+        case 2:
+            printf("*******************Inches To Foot*******************\n");
+            printf("Enter the value which you want to convert\n");
+            scanf("%f", &b);
+            printf("%d inches = %f foots\n", (int)b, b * 0.0833333);
+
+            break;
+        case 3:
+            printf("*******************Cms To Inches*******************\n");
+            printf("Enter the value which you want to convert\n");
+            scanf("%f", &b);
+            printf("%d cms = %f foots\n", (int)b, b * 0.0833333);
+
+            break;
+        case 4:
+            printf("*******************Pound To Kgs*******************\n");
+            printf("Enter the value which you want to convert\n");
+            scanf("%f", &b);
+            printf("%d pound = %f kgs\n", (int)b, b * 0.453592);
+
+            break;
+        case 5:
+            printf("*******************Inches To Meters*******************\n");
+            printf("Enter the value which you want to convert\n");
+            scanf("%f", &b);
+            printf("%d inches = %f meters\n", (int)b, b * 0.0254);
+
+        default:
+            printf("quit");
+        }
+    }
+    end:
+    return 0;
+}
