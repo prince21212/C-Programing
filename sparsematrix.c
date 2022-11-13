633df09d88e34aeafe8896b67fd96a63054d47b1
prince kumar <113275797+prince21212@users.noreply.github.com>
2022-11-14 01:02:32 +0530

    sparse matrix program using c language

---
+++ sparsematrix.c
@@ -0,0 +1,53 @@
+// C++ program for Sparse Matrix Representation
+// using Array
+#include<stdio.h>
+
+int main()
+{
+	// Assume 4x5 sparse matrix
+	int sparseMatrix[4][5] =
+	{
+		{0 , 0 , 3 , 0 , 4 },
+		{0 , 0 , 5 , 7 , 0 },
+		{0 , 0 , 0 , 0 , 0 },
+		{0 , 2 , 6 , 0 , 0 }
+	};
+
+	int size = 0;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 5; j++)
+			if (sparseMatrix[i][j] != 0)
+				size++;
+
+	// number of columns in compactMatrix (size) must be
+	// equal to number of non - zero elements in
+	// sparseMatrix
+	int compactMatrix[3][size];
+
+	// Making of new matrix
+	int k = 0;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 5; j++)
+			if (sparseMatrix[i][j] != 0)
+			{
+				compactMatrix[0][k] = i;
+				compactMatrix[1][k] = j;
+				compactMatrix[2][k] = sparseMatrix[i][j];
+				k++;
+			}
+
+	for (int i=0; i<3; i++)
+	{
+		for (int j=0; j<size; j++)
+			printf("%d ", compactMatrix[i][j]);
+
+		printf("\n");
+	}
+	return 0;
+}
+/*
+OUTPUT :
+0 0 1 1 3 3 
+2 4 2 3 1 2
+3 4 5 7 2 6
+*/
